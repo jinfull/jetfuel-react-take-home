14e78eb093d259e5fe110600c9cf4261f438ff88
jinfull <jinfull@gmail.com>
2019-06-26 16:27:23 -0700

    Experiment with error handling of failed API request

--- app/src/CampaignList.js
+++ app/src/CampaignList.js
@@ -4,19 +4,10 @@ import CampaignItem from './CampaignItem';
 class CampaignList extends React.Component {
   constructor(props) {
     super(props);
-    // this.requestSuccess = null;
     this.state = {};
   }
 
   componentDidMount() {
-    let xhr = fetch('https://www.plugco.in/public/take_home_sample_feed');
-
-    console.log(xhr);
-
-    // if (xhr[[PromiseStatus]] === "rejected") {
-    //   return null;
-    // }
-
     fetch('https://www.plugco.in/public/take_home_sample_feed')
       .then(response => response.json())
       .then(data => this.setState({ data }))
@@ -26,18 +17,18 @@ class CampaignList extends React.Component {
   render() {
     if (!this.state.data) return null;
 
+    // if (!this.state.data) {
+    //   return (
+    //     <div>Sorry! No campaigns to display!</div>
+    //   )
+    // }
+
     let campaigns = this.state.data.campaigns.map(campaign => (
       <CampaignItem
         campaign={campaign}
         key={campaign.id}
       />
     ));
-
-    // if (!this.requestSuccess) {
-    //   return (
-    //     <div className='apiBroke'>Sorry, wrong page!</div>
-    //   )
-    // }
 
     return (
       <ul className='campaign-ul'>
