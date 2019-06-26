93dfe686fb0a088e924d212d5e51e00fe89c1343
jinfull <jinfull@gmail.com>
2019-06-26 02:19:40 -0700

    Add copytoclipboard feature, minor polishing and styling changes

--- app/src/CampaignItem.js
+++ app/src/CampaignItem.js
@@ -10,7 +10,7 @@ class CampaignItem extends React.Component {
     return (
       <li className='campaign-li'>
         <div className='campaign-info'>
-          <img className='campaign-logo' src={campaign.campaign_icon_url}></img>
+          <img className='campaign-logo' src={campaign.campaign_icon_url} alt='campaign logo'></img>
           <div className='campaign-details'>
             <div className='campaign-name'>{campaign.campaign_name}</div>
             <div className='campaign-pay'><strong>{campaign.pay_per_install}</strong> per install</div>
--- app/src/MediaItem.js
+++ app/src/MediaItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class MediaItem extends React.Component {
   render() {
@@ -8,10 +9,12 @@ class MediaItem extends React.Component {
 
     return (
       <li className='media-li'>
-        <a href={media.download_url}><img className='media-preview' src={media.cover_photo_url}></img></a>
+        <a href={media.download_url}><img className='media-preview' src={media.cover_photo_url} alt='media preview'></img></a>
         <div className='media-bottom-row'>
-          <div className='copy-link'></div>
-          <div className='download-url'></div>
+          <CopyToClipboard text={media.tracking_link}>
+            <img className='media-button' src={require('./assets/link.png')} alt='link button'></img>
+          </CopyToClipboard>
+          <a href={media.download_url} download><img className='media-button' src={require('./assets/download.png')} alt='download button'></img></a>
         </div>
       </li>
     )
